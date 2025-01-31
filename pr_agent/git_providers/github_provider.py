5b36916f32e08e9cbdeb2396d6a2d7b588f335c5
Praveen Raj <praveen@tazapay.com>
2025-01-31 15:46:29 +0530

    read local .pr_agent.toml

--- pr_agent/git_providers/github_provider.py
+++ pr_agent/git_providers/github_provider.py
@@ -4,6 +4,7 @@ import hashlib
 import itertools
 import re
 import time
+import os
 import traceback
 from datetime import datetime
 from typing import Optional, Tuple
@@ -598,6 +599,7 @@ class GithubProvider(GitProvider):
     def get_issue_comments(self):
         return self.pr.get_issue_comments()
 
+    """
     def get_repo_settings(self):
         try:
             # contents = self.repo_obj.get_contents(".pr_agent.toml", ref=self.pr.head.sha).decoded_content
@@ -606,6 +608,17 @@ class GithubProvider(GitProvider):
             contents = self.repo_obj.get_contents(".pr_agent.toml").decoded_content
             return contents
         except Exception:
+            return ""
+    """
+
+    def get_repo_settings(self):
+        try:
+            file_path = os.path.join(os.getcwd(), ".pr_agent.toml")
+            with open(file_path, "r", encoding="utf-8") as file:
+                contents = file.read()
+            return contents
+        except Exception as e:
+            get_logger().warning(f"Failed to read local .pr_agent.toml: {e}")
             return ""
 
     def get_workspace_name(self):
